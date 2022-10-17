a790f53a7f2ff53bca86049c70e0fb58dd667a54
braun07 <joaovitorbraun6@gmail.com>
2022-10-16 22:39:13 -0300

    intro paginas dinamicas e carregamentos dinamicos

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 // nossodominio.com/news/something-important
 
 function DetailPage() {
-    return <h1> the detail page</h1>
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+  //envia um request pra API do backend
+  //pra dar match nos novos itens com o newsId no database
+
+    return <h1> the detail page</h1>;
 }
 
 export default DetailPage;
