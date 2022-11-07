28f47e0035b8043a4d4bff1e3e8cfbcf4a993566
braun07 <joaovitorbraun6@gmail.com>
2022-11-07 16:14:35 -0300

    sistema de navegação com LINK

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,29 @@
 // nossodominio.com/news
 
+import Link from "next/link";
+import { Fragment } from "react";
+
 // nossodominio.com/
 
 function NewsPage() {
-    return <h1> the news page</h1>
+    return (
+        <Fragment>
+        <h1> the news page</h1>
+        <ul>
+            <li>
+                <Link href="/news/nextjs-is-a-great-framework">
+                    NextJS Is a Great Framework
+                </Link>
+            </li>
+            <li>
+                <Link href="/news/something-else">
+                    Something Else
+                </Link>
+            </li>
+        </ul>
+        </Fragment>
+    )
+
 }
 
 export default NewsPage;
